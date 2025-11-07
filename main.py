e72638e36bfd25a0a949cd41134d11a6530349f0
Lolikion228 <st117139@student.spbu.ru>
2025-11-07 12:02:04 +0300

    init

---
+++ main.py
@@ -0,0 +1,43 @@
+import numpy as np
+from scipy import stats
+
+#график лог-длины интервала относительно n при фикс уровне доверия
+#график лог-длины интервала относительно уровня доверия при фикс n
+
+a = 11.666
+sigma = 1.7
+
+#for a with known sigma^2
+def ci1(X, eps, sigma):
+    mean = np.mean(X)
+    tau = stats.norm.ppf( (1 + eps) / 2 )
+    n = X.shape[0]
+    return [ mean - tau * sigma / (n**0.5), mean + tau * sigma / (n**0.5) ]
+    
+
+#for sigma^2 with known a
+def ci2(X, eps, a):
+    n = X.shape[0]
+    g2 = stats.chi2.ppf( (1 + eps) / 2, df=n)
+    g1 = stats.chi2.ppf( (1 - eps) / 2, df=n)
+    S_1 =  np.mean( (X - a) ** 2 )
+    return [ n * S_1 / g2, n * S_1 / g1 ]
+
+
+#for a with unknown sigma^2
+def ci3(X, eps):
+    mean = np.mean(X)
+    tau = stats.t.ppf( (1 + eps) / 2, df=n-1)
+    n = X.shape[0]
+    S_0 =   np.mean((X -mean)**2) * (n / (n-1))
+    return [ mean - tau * S_0 / (n**0.5), mean + tau * S_0 / (n**0.5) ]
+
+#for sigma^2 with uknown a
+def ci4(X, eps):
+    n = X.shape[0]
+    g2 = stats.chi2.ppf( (1 + eps) / 2, df=n-1)
+    g1 = stats.chi2.ppf( (1 - eps) / 2, df=n-1)
+    S_0 =   np.mean((X -np.mean(X))**2) * (n / (n-1))
+    return [ (n-1) * S_0 / g2, (n-1) * S_0 / g1 ]
+
+
